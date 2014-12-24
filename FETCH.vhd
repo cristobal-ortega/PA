18a25e67a10d874b7d6bb6eece5c6ad5f1a5ddc2
David Trilla <dtrilla@linux-cwyr.site>
2014-12-24 21:00:34 +0100

    update

--- FETCH.vhd
+++ FETCH.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
 
 ENTITY FETCH IS 
 	PORT (clock : IN	STD_LOGIC;
@@ -7,8 +8,18 @@ ENTITY FETCH IS
 
 END FETCH;
 
-
 ARCHITECTURE Structure OF FETCH IS
+	COMPONENT inst_cache
+	PORT (clock : IN	STD_LOGIC;
+			pc : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			inst  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+	END COMPONENT;
+	
+	signal pc : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	
 BEGIN
-
+	IC: inst_cache
+	PORT MAP(clock => clock,
+				pc => pc,
+				inst => inst);
 END Structure;
