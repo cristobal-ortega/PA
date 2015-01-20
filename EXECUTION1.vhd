0dcc2da682b7b3bf741de34cf40e1564e6a69d2c
davidtrilla <dtrilla92@gmail.com>
2015-01-20 18:45:41 +0100

    alu

--- EXECUTION1.vhd
+++ EXECUTION1.vhd
@@ -1,6 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-USE ieee.std_logic_unsigned.all;
+
 
 
 ENTITY EXECUTION1 IS 
@@ -8,16 +8,29 @@ ENTITY EXECUTION1 IS
 			op  : IN STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
 			a : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
 			b : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-			w	 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000");
+			w	 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			z : OUT STD_LOGIC);
 
 END EXECUTION1;
 
 
 ARCHITECTURE Structure OF EXECUTION1 IS
+
+	COMPONENT ALU
+	PORT (x  : IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
+			y  : IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
+			w  : OUT	STD_LOGIC_VECTOR(15 DOWNTO 0);
+			op : IN	STD_LOGIC_VECTOR(3 DOWNTO 0);
+			z  : OUT STD_LOGIC);
+	END COMPONENT;
 BEGIN
 -- Aqui va una alu, => encapsular esto dfe aqui aabjo
-	WITH op SELECT w <= 
-		a+b WHEN "0100",
-		a-b WHEN "0101",
-		a+b WHEN OTHERS;
+	ALU1: ALU
+	PORT MAP(x => a,
+				y => b,
+				w => w,
+				op => op,
+				z => z);
+	
+
 END Structure;
--- alu.vhd
+++ alu.vhd
@@ -1,20 +1,35 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-USE IEEE.numeric_std.all;
+USE ieee.std_logic_unsigned.all;
 
-ENTITY alu IS 
+ENTITY ALU IS 
 	PORT (x  : IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
 			y  : IN	STD_LOGIC_VECTOR(15 DOWNTO 0);
 			w  : OUT	STD_LOGIC_VECTOR(15 DOWNTO 0);
-			op : IN	STD_LOGIC);
-END alu;
+			op : IN	STD_LOGIC_VECTOR(3 DOWNTO 0);
+			z  : OUT STD_LOGIC);
+END ALU;
 
 
-ARCHITECTURE Structure OF alu IS
+ARCHITECTURE Structure OF ALU IS
+
+	SIGNAL waux : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL add  : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL sub  : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	
 BEGIN
+	add <= x+y;
+	sub <= x-y;
 
-WITH op SELECT 
-	w <= y(15 DOWNTO 0) WHEN '0',
-		  y(7 DOWNTO 0)&x(7 DOWNTO 0) WHEN OTHERS;
+	WITH op SELECT waux <= 
+		add WHEN "0100", --add
+		sub WHEN "0101", --sub
+		sub WHEN "0110", --cmp
+		sub WHEN "0111", --bnz
+		add WHEN OTHERS;
+		
+	--Mucho ojo con esto: si z ejecuta antes k se ponga w, esto va a dar mal, hay que testear
+	z <= not (waux(0) or waux(1) or waux(2) or waux(3) or waux(4) or waux(5) or waux(6) or waux(7) or waux(8) or waux(9) or waux(10) or waux(11) or waux(13) or waux(14) or waux(15) );
+	w <= waux;
 	
 END Structure;
