f90fa862e54a85ebfa847a628e0549c231e75f95
Frankicia <nope@example.com>
2015-01-21 21:54:48 +0100

    Decode with correct outputs

--- DECODE1.vhd
+++ DECODE1.vhd
@@ -5,14 +5,19 @@ USE ieee.std_logic_unsigned.all;
 
 ENTITY DECODE1 IS 
 	PORT (clock : IN	STD_LOGIC;
-			e_write : IN STD_LOGIC; -- Permision to write in the register bank
-			regaddr : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			e_writeBR : IN STD_LOGIC; -- Permision to write in the register bank
+			regDST : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
 			inst  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-			regwrite	: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-			e_writedecod : OUT STD_LOGIC; -- Decoded instruction writes the regiter bank
+			w	: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			
+			e_writeBR_out : OUT STD_LOGIC; -- Decoded instruction writes the regiter bank
 			op		: OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-			reg_a : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-			reg_b : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+			reg_a : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			reg_b : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			regDST : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			PC : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			inst_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+			);
 
 END DECODE1;
 
@@ -26,7 +31,7 @@ BEGIN
 	op <= inst(15 DOWNTO 12);
 	reg_a <= reg_bank(CONV_INTEGER(inst(11 DOWNTO 8))); 
 	reg_b <= reg_bank(CONV_INTEGER(inst(7 DOWNTO 4))); 
-	WITH inst(15 DOWNTO 12) SELECT e_writedecod <= 
+	WITH inst(15 DOWNTO 12) SELECT e_writeBR_out <= 
 													'1' WHEN "0100", --add
 													'1' WHEN "0101", --sub
 													'1' WHEN "0001", --lb
@@ -34,8 +39,8 @@ BEGIN
 	
 	PROCESS(clock)
 	BEGIN
-		IF clock = '0' AND e_write = '1' THEN
-			reg_bank(CONV_INTEGER(regaddr)) <= regwrite;
+		IF clock = '0' AND e_writeBR = '1' THEN
+			reg_bank(CONV_INTEGER(regDST)) <= w;
 		END IF;
 	END PROCESS;
 
