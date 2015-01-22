2e22a685aa2f29b9ad2204ef091ea360397ef0e9
Frankicia <nope@example.com>
2015-01-22 17:52:47 +0100

    registros de desacoplo con toda la info

--- DECODE1_EXECUTION1.vhd
+++ DECODE1_EXECUTION1.vhd
@@ -4,17 +4,25 @@ USE ieee.std_logic_1164.all;
 ENTITY DECODE1_EXECUTION1 IS 
 	PORT (clock    : IN	STD_LOGIC;
 			stall    : IN STD_LOGIC;
-			e_writeBR : IN STD_LOGIC;
-			op_in    : IN STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
-			a_in  : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-			b_in  : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			
+			
+			e_writeBR_in : IN STD_LOGIC;
+			e_writeBR_long_in : IN STD_LOGIC;
+			op_in		: IN STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
+			a_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			b_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			regDST_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			PC_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			inst_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
+			
 			e_writeBR_out : OUT STD_LOGIC;
+			e_writeBR_long_out : OUT STD_LOGIC;
 			op_out   : OUT STD_LOGIC_VECTOR(3 DOWNTO 0) :="0000";
 			a_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
 			b_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-			regDST : OUT  STD_LOGIC_VECTOR(3 DOWNTO 0);
-			PC : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-			inst_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+			regDST_out : OUT  STD_LOGIC_VECTOR(3 DOWNTO 0);
+			PC_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			inst_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
 			);
 
 END DECODE1_EXECUTION1;
@@ -22,25 +30,36 @@ END DECODE1_EXECUTION1;
 
 ARCHITECTURE Structure OF DECODE1_EXECUTION1 IS
 
-	SIGNAL ewritedecod_reg : STD_LOGIC;
-	SIGNAL op_reg : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
-	SIGNAL rega_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-	SIGNAL regb_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL e_writeBR_reg : STD_LOGIC;
+	SIGNAL op_reg   : STD_LOGIC_VECTOR(3 DOWNTO 0) :="0000";
+	SIGNAL a_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL b_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL regDST_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL PC_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL inst_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
 BEGIN
 	
-	ewritedecod_out <= ewritedecod_reg;
+
+	e_writeBR_out <= e_writeBR_reg;
 	op_out <= op_reg;
-	rega_out <= rega_reg;
-	regb_out <= regb_reg;
+	a_out <= a_reg;
+	b_out <= b_reg;
+	regDST_out <= regDST_reg;
+	PC_out <= PC_reg;
+	inst_out <= inst_reg;
 	
 	PROCESS(clock)
 	BEGIN
 		IF (RISING_EDGE(clock)) AND (stall = '0')  THEN
-			ewritedecod_reg <= ewritedecod_in;
+			e_writeBR_reg <= e_writeBR_reg;
 			op_reg <= op_in;
-			rega_reg <= rega_in;
-			regb_reg <= regb_in;
+			a_reg <= a_in;
+			b_reg <= b_in;
+			regDST_reg <= regDST_in;
+			PC_reg <= PC_in;
+			inst_reg <= inst_in;
+	
 		END IF;
 	END PROCESS;
 END Structure;
--- EXECUTION1_MEMORY1.vhd
+++ EXECUTION1_MEMORY1.vhd
@@ -4,28 +4,54 @@ USE ieee.std_logic_1164.all;
 ENTITY EXECUTION1_MEMORY1 IS 
 	PORT (clock : IN	STD_LOGIC;
 			stall : IN STD_LOGIC;
-			ewritedecod_in : IN STD_LOGIC;
+			
+			e_writeBR_in : IN STD_LOGIC;
 			w_in  : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-			ewritedecod_out : OUT STD_LOGIC;
-			w_out	 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000");
+			regDST_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			PC_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			instr_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			op_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			
+			e_writeBR_out : OUT STD_LOGIC;
+			w_out	 : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			regDST_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			PC_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+			instr_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+			op_out : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+			
+			
+			);
 
 END EXECUTION1_MEMORY1;
 
 
 ARCHITECTURE Structure OF EXECUTION1_MEMORY1 IS
+	
+	SIGNAL e_writeBR_reg : STD_LOGIC;
+	SIGNAL op_reg   : STD_LOGIC_VECTOR(3 DOWNTO 0) :="0000";
+	SIGNAL regDST_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL PC_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
+	SIGNAL inst_reg : STD_LOGIC_VECTOR(3 DOWNTO 0);
 	SIGNAL w_reg : STD_LOGIC_VECTOR(15 DOWNTO 0) := "0000000000000000";
-		SIGNAL ewritedecod_reg : STD_LOGIC;
 
 BEGIN
 
-	ewritedecod_out <= ewritedecod_reg;
+	e_writeBR_out <= e_writeBR_reg;
 	w_out <= w_reg;
+	regDST_out <= regDST_reg;
+	PC_out <= PC_reg;
+	instr_out <= inst_reg;
+	op_out <= op_reg;
 	
 	PROCESS(clock)
 	BEGIN
 		IF (RISING_EDGE(clock)) AND (stall = '0')  THEN
-			ewritedecod_reg <= ewritedecod_in;
+			e_writeBR_reg <= e_writeBR_reg;
 			w_reg <= w_in;
+			op_reg <= op_in;
+			regDST_reg <= regDST_in;
+			PC_reg <= PC_in;
+			inst_reg <= instr_in;
 		END IF;
 	END PROCESS;
 
